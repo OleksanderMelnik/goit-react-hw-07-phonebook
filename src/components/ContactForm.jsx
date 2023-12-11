86692d956d3a06729d7043f88f07ab94bedf552e
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-12-11 10:51:41 +0200

    bugfix/contact matching check

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,30 +1,62 @@
-import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
 import { Form, Label, Button, Input } from './ContactsForm.styled'
 import { addContactThunk } from 'redux/contactsThunk';
+import { getFilteredContacts } from 'redux/selectors';
 
 export const ContactForm = () => {
   
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const contacts = useSelector(getFilteredContacts);
+ 
   const dispatch = useDispatch(); 
 
-  const handleChange = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    const newObj = {
-      name: event.target.elements.name.value,
-      number: event.target.elements.number.value,
+    const filterContact = contacts.some(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
+    );
+
+    if (filterContact) {
+      alert(`${name} is already in contacts`);
+      resetInput();
+    return;
+}
+
+    dispatch(addContactThunk({ name, number }));
+    resetInput();
     };
- 
-    dispatch(addContactThunk(newObj));
-    event.target.reset();
-    
-  };
+
+    const handleChange = event => {
+      const { name, value } = event.target;
+      switch (name) {
+        case 'name':
+          setName(value);
+          break;
+        case 'number':
+          setNumber(value);
+          break;
+        default:
+          return;
+      }
+    };
+
+    const resetInput = () => {
+      setName('');
+      setNumber('');
+    };
 
   return (
-    <Form onSubmit={handleChange}>
+    <Form onSubmit={handleSubmit}>
       <Label htmlFor="">
         Name
         <Input
+          value={name}
+          onChange={handleChange}
           type="text"
           name="name"
           title=""
@@ -35,6 +67,8 @@ export const ContactForm = () => {
       <Label htmlFor="">
         Number
         <Input
+          value={number}
+          onChange={handleChange}
           type="tel"
           name="number"
           title=""
